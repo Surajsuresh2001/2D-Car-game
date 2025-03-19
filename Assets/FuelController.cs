ec813011b170fc55e4b16882894ba04c157c1968
Suraj Suresh <surajsuresh293@gmail.com>
2025-03-19 19:39:50 +0530

    2D Car Game

---
+++ Assets/FuelController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FuelController : MonoBehaviour
+{
+    public static FuelController instance;
+
+    [SerializeField] private Image _fuelImage;
+    [SerializeField, Range(0.1f, 5f)] private float _fuelDrainSpeed = 10f;
+    [SerializeField] private float _maxFuelAmount = 100f;
+    [SerializeField] private Gradient _fuelGradient;
+
+    private float _currentFuelAmount;
+    
+
+    // Update is called once per frame
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        _currentFuelAmount = _maxFuelAmount;
+        UpdateUI();
+    }
+
+    private void Update()
+    {
+        _currentFuelAmount -= Time.deltaTime * _fuelDrainSpeed;
+        UpdateUI();
+        
+        if (_currentFuelAmount <= 0f)
+        {
+            GameManager.instance.GameOver();
+        }
+    }
+
+    private void UpdateUI()
+    {
+        _fuelImage.fillAmount = (_currentFuelAmount / _maxFuelAmount);
+        _fuelImage.color = _fuelGradient.Evaluate(_fuelImage.fillAmount);
+    }
+
+    public void FillFuel()
+    {
+        _currentFuelAmount = _maxFuelAmount;
+        UpdateUI();
+    }
+}
